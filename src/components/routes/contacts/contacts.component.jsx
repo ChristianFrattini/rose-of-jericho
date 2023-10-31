387bcd44b09fc6adf515678c92c75eb8ad774ad2
chrifrat1 <chrifrat1@gmail.com>
2023-10-31 16:23:55 +0100

    dark mode correction

--- src/components/routes/contacts/contacts.component.jsx
+++ src/components/routes/contacts/contacts.component.jsx
@@ -6,7 +6,7 @@ const Contacts =()=>{
       <h2>Contact Us</h2>
       <h4>Please, if you are contacting us regarding a placed order, include your name and order number.</h4>
       <p>
-        Email: <a href="mailto:your.email@example.com">your.email@example.com</a>
+        Email: <a href="mailto:contactroseofjericho@gmail.com">contactroseofjericho@gmail.com</a>
       </p>
       <p>
         Instagram: <a href="https://www.instagram.com/yourprofile">yourprofile</a>
