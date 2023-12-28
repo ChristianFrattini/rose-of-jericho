d07b497c850a50d7c9d7d709f86d70bda15bcf77
chrifrat1 <chrifrat1@gmail.com>
2023-12-28 19:23:52 +0100

    purchase link

--- src/components/routes/purchase/purchase.component.jsx
+++ src/components/routes/purchase/purchase.component.jsx
@@ -4,9 +4,9 @@ const Purchase =()=>{
     return (
         <div className="purchase-info">
       <h2>Purchase</h2>
-      <h3>You can buy your Rose of Jericho on Amazon. Please, follow the link below:</h3>
+      <h3>You can buy your Rose of Jericho on Shopify. Please, follow the link below:</h3>
       <p>
-        Amazon: <a href="https://www.amazon.co.uk/" target='blank'>Purchase your Rose of Jericho here</a>
+        Shopify: <a href="https://5262b3-4.myshopify.com/products/the-rose-of-jericho-resurrection-plant-perfect-for-home-and-office-decoration?utm_source=copyToPasteBoard&utm_medium=product-links&utm_content=web" target='blank'>Purchase your Rose of Jericho here</a>
       </p>
     </div>
     )
